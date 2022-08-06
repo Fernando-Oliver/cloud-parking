44e02724dcff51810c8081665e9d2a5e10fd5300
Fernando de Oliveira Carvalho <fernandooliveiracarvalho98@gmail.com>
2022-08-06 18:41:17 -0300

    Implementação do TestContainers

--- src/test/java/com/one/digitalinnovation/cloudparking/ParkingControllerIT.java
+++ src/test/java/com/one/digitalinnovation/cloudparking/ParkingControllerIT.java
@@ -13,7 +13,7 @@ import com.one.digitalinnovation.cloudparking.DTO.ParkingCreateDTO;
 import io.restassured.RestAssured;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ParkingControllerIT {
+class ParkingControllerIT extends AbstractContainerBase {
 
 	@LocalServerPort
 	private int randomPort;
